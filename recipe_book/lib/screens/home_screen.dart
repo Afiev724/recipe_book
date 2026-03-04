82e4cf55d58308bc1da17db763467b8ff4f9bcc8
aaliyah fievre <aaliyahfievre@gmail.com>
2026-03-04 15:15:28 -0500

    had to debug pages

--- recipe_book/lib/screens/home_screen.dart
+++ recipe_book/lib/screens/home_screen.dart
@@ -8,26 +8,27 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Recipe Book')),
-      body:
-
-ListView.builder(
-  itemCount: sampleRecipes.length,
-  itemBuilder: (context, index) {
-    final recipe = sampleRecipes[index];
-    return Card(
-      child: ListTile(
-        leading: Image.asset(recipe.imagePath, width: 56, fit: BoxFit.cover),
-        title:   Text(recipe.name),
-        trailing: const Icon(Icons.chevron_right),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => DetailsScreen(recipe: recipe),
+      body: ListView.builder(
+        itemCount: sampleRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = sampleRecipes[index];
+          return Card(
+            child: ListTile(
+              leading: Image.asset(recipe.imagePath, width: 56, fit: BoxFit.cover),
+              title:   Text(recipe.name),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DetailsScreen(recipe: recipe),
+                  ),
+                );
+              },
             ),
           );
         },
       ),
     );
-  },
-)
+  }
+}
