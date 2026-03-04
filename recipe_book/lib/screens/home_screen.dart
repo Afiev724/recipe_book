fbdf7b4c18bc0f2ee43f18129a5e8f1de826f923
aaliyah fievre <aaliyahfievre@gmail.com>
2026-03-04 15:02:15 -0500

    implemented recipe model and screens: details, favorites, and home

---
+++ recipe_book/lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:recipe_book/data/recipes_data.dart';
+import 'package:recipe_book/screens/details_screen.dart';
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Recipe Book')),
+      body:
+
+ListView.builder(
+  itemCount: sampleRecipes.length,
+  itemBuilder: (context, index) {
+    final recipe = sampleRecipes[index];
+    return Card(
+      child: ListTile(
+        leading: Image.asset(recipe.imagePath, width: 56, fit: BoxFit.cover),
+        title:   Text(recipe.name),
+        trailing: const Icon(Icons.chevron_right),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => DetailsScreen(recipe: recipe),
+            ),
+          );
+        },
+      ),
+    );
+  },
+)
